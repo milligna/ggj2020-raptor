18ad9f3263578b86e5ea95f936bd9596f4a0744e
Peter J Armstrong <ashen_s@yahoo.com.au>
2020-02-01 13:04:14 +1100

    Scripts for computer and player

---
+++ Raptor Prison Ship/Assets/Scripts/Computer.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ComputerType {
+	Mac,
+	Windows,
+	AppleIIe,
+	Commodore64
+};
+
+public class Computer : MonoBehaviour
+{
+	public enum ComputerState {
+		WaitingToCrash,
+		Crashed,
+		SafelyOccupied,
+		Exploding
+	}
+
+	public int computerID;
+	public ComputerType computerType;
+	public ComputerState _state;
+	private float _crashTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		if (computerID == 0) {
+			computerID = Random.Range (0, 100);
+		}
+		computerType =  (ComputerType)Random.Range (0, System.Enum.GetValues(typeof(ComputerType)).Length);
+		_state = ComputerState.WaitingToCrash;
+		_crashTimer = Random.Range (3f, 20f);
+    }
+
+	private void OnTriggerEnter (Collider other)
+	{
+		// If we've already clicked on the computer before being in range
+		if (other.gameObject.layer == LayerMask.NameToLayer("Player") ) {
+			if (other.gameObject.GetComponent<InputControl> ().targettedComputer == computerID) {
+				rebootComputer ();
+			}
+		}
+	}
+
+
+	private void crashComputer ()
+	{
+		SetComputerColour (Color.magenta);
+		_crashTimer = Random.Range (10f, 20f);
+
+	}
+
+	private void SetComputerColour (Color colour)
+	{
+		this.gameObject.GetComponent<Renderer> ().material.color = colour;
+
+	}
+
+	public void rebootComputer ()
+	{
+		if (_state == ComputerState.Crashed) {
+			_state = ComputerState.WaitingToCrash;
+			_crashTimer = Random.Range (3f, 20f);
+
+			SetComputerColour (Color.white);
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		_crashTimer -= Time.deltaTime;
+
+		if (_crashTimer < 0) {
+			if (_state == ComputerState.WaitingToCrash) {
+				_state = ComputerState.Crashed;
+				crashComputer ();
+			} else if (_state == ComputerState.Crashed) {
+				_state = ComputerState.Exploding;
+				SetComputerColour (Color.black);
+			}
+		}
+    }
+}
---
+++ Raptor Prison Ship/Assets/Scripts/InputControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class InputControl : MonoBehaviour
+{
+	public NavMeshAgent player;
+
+	public float PlayerComputerInteraction = 2.0f;	// Should be about the same as the computer's trigger colliders diameter/radius??
+
+	public int targettedComputer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		targettedComputer = 0;	// Not aiming at a computer
+    }
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray raything = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit rayResult;
+
+			if (Physics.Raycast (raything, out rayResult)) {
+				player.SetDestination (rayResult.point);
+
+				if (rayResult.collider.gameObject.layer == LayerMask.NameToLayer ("Computer")) {
+					Computer touchedComputer = rayResult.collider.gameObject.GetComponent<Computer> ();
+					targettedComputer = touchedComputer.computerID;
+					if ((player.destination - player.gameObject.transform.position).magnitude < PlayerComputerInteraction ) {
+						touchedComputer.rebootComputer ();
+					}
+				} else {
+					targettedComputer = 0;
+				}
+
+
+			}
+
+		}
+    }
+}
